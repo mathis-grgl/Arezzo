224ed7163580fdbb0337273a02569d61af629d02
georgel27u <georhyunj>
2022-04-29 10:01:23 +0200

    Organisation des dossiers et fichiers, création de l'interface du menu

--- Arezzo/src/Main.java
+++ Arezzo/src/Main.java
@@ -1,18 +1,19 @@
-package sample;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        BorderPane root = new BorderPane();
+        Parent menu = FXMLLoader.load(getClass().getResource("fxml/menu.fxml"));
+        root.setTop(menu);
+        primaryStage.setTitle("Arezzo");
+        primaryStage.setScene(new Scene(root, 600, 550));
         primaryStage.show();
     }
 
---
+++ Arezzo/src/vues/VueMenu.java
@@ -0,0 +1,20 @@
+package vues;
+
+import javafx.fxml.FXML;
+
+
+public class VueMenu {
+
+    public VueMenu(){
+        System.out.println("Test");
+    }
+
+    @FXML
+    public void testT(){
+        System.out.println("Jessaie");
+    }
+}
+
+
+
+
