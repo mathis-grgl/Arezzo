c89ba660e8caf08509f71ba7d4c273dc0f656c3d
georgel27u <georhyunj>
2022-05-12 16:24:22 +0200

    Enregistrer une mélodie en json.

--- arezzo/src/ecouteur/EcouteurMenu.java
+++ arezzo/src/ecouteur/EcouteurMenu.java
@@ -4,10 +4,21 @@ import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextInputDialog;
+import javafx.stage.FileChooser;
+import javafx.stage.Popup;
 import model.Arezzo;
+import netscape.javascript.JSObject;
 import partition.Partition;
 
+import java.awt.event.MouseEvent;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
 import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 public class EcouteurMenu implements Observateur{
@@ -30,10 +41,43 @@ public class EcouteurMenu implements Observateur{
 
     @FXML
     public void ouvrirFichier(){
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setInitialFileName(arezzo.getTitre()+".json");
+
+        Popup pop = new Popup();
+        File file = fileChooser.showOpenDialog(pop);
+
+        try {
+            String contenu = Files.readString(file.toPath());
+            Pattern pattern = Pattern.compile("\"\\*\",");
+            Matcher matcher = pattern.matcher(contenu);
+            String titre = matcher.group();
+            System.out.println(titre);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @FXML
-    public void enregistrerSous(){}
+    public void enregistrerSous() {
+
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setInitialFileName(arezzo.getTitre()+".json");
+
+        Popup pop= new Popup();
+        File file = fileChooser.showSaveDialog(pop);
+
+        String contenu = "{ \"titre\":\"" + arezzo.getTitre() + "\",\"melodie\":\"" + arezzo.getMelodie() + "\" }";
+
+        try {
+            PrintWriter writer;
+            writer = new PrintWriter(file);
+            writer.println(contenu);
+            writer.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 
     @FXML
     public void quitterFenetre(){
--- arezzo/src/model/Arezzo.java
+++ arezzo/src/model/Arezzo.java
@@ -142,4 +142,8 @@ public class Arezzo extends SujetObserve {
     public void setTitre(String titre) {
         this.titre = titre;
     }
+
+    public String getMelodie() {
+        return melodie;
+    }
 }
